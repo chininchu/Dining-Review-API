1ca232494466e64427303d026b2b19541ccbb02c
chininchu <120990876+chininchu@users.noreply.github.com>
2023-07-20 21:02:42 -0500

    Chnaged artificat ID and name

---
+++ src/main/java/com/codeup/diningreviewapi/DiningReviewApiApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.diningreviewapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiningReviewApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DiningReviewApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/codeup/diningreviewapi/DiningReviewApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeup.diningreviewapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiningReviewApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
