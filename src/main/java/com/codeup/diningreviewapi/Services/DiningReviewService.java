847145473a11083121b528a49e2da9f3680eb20e
chininchu <120990876+chininchu@users.noreply.github.com>
2023-08-03 16:56:45 +0500

    Created a method called getPendingDiningReviews

--- src/main/java/com/codeup/diningreviewapi/Services/DiningReviewService.java
+++ src/main/java/com/codeup/diningreviewapi/Services/DiningReviewService.java
@@ -1,10 +1,13 @@
 package com.codeup.diningreviewapi.Services;
 
+import com.codeup.diningreviewapi.enums.ReviewStatus;
 import com.codeup.diningreviewapi.models.DiningReview;
 import com.codeup.diningreviewapi.repositories.DiningReviewRepository;
 import com.codeup.diningreviewapi.repositories.DiningUserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class DiningReviewService {
@@ -27,6 +30,15 @@ public class DiningReviewService {
         // (Scenario: As a registered user, I want to submit a dining review)
 
         return diningReviewRepository.save(diningReview);
+
+    }
+
+    public List<DiningReview> getPendingDiningReviews() {
+
+        // Implement logic to fetch all dining reviews that are pending approval
+        // (Scenario: As an admin, I want to get the list of all dining reviews that are pending approval)
+        return diningReviewRepository.findByStatus(ReviewStatus.PENDING);
+
 
     }
 
