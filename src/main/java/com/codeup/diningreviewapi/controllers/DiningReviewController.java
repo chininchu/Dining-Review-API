52e00fd3b11d8f615a432fbbfe1cb8d1d13be08b
chininchu <120990876+chininchu@users.noreply.github.com>
2023-08-04 20:14:11 +0500

    Created a @PostMapping method called submitDiningReview

--- src/main/java/com/codeup/diningreviewapi/controllers/DiningReviewController.java
+++ src/main/java/com/codeup/diningreviewapi/controllers/DiningReviewController.java
@@ -1,7 +1,10 @@
 package com.codeup.diningreviewapi.controllers;
 
 import com.codeup.diningreviewapi.Services.DiningReviewService;
+import com.codeup.diningreviewapi.models.DiningReview;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,6 +19,15 @@ public class DiningReviewController {
     public DiningReviewController(DiningReviewService diningReviewService) {
 
         this.diningReviewService = diningReviewService;
+
+
+    }
+
+    @PostMapping
+    public DiningReview submitDiningReview(@RequestBody DiningReview diningReview){
+
+        return diningReviewService.submitDiningReview(diningReview);
+
 
 
     }
