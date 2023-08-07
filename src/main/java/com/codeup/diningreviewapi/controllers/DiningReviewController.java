70ea49c5dc18db3b797d66b37e33234c619a94a0
chininchu <120990876+chininchu@users.noreply.github.com>
2023-08-07 11:11:12 +0500

    Created two methods within the DiningReviewController 1. getPendingReviews and approveOrRejectDiningReview

--- src/main/java/com/codeup/diningreviewapi/controllers/DiningReviewController.java
+++ src/main/java/com/codeup/diningreviewapi/controllers/DiningReviewController.java
@@ -2,11 +2,11 @@ package com.codeup.diningreviewapi.controllers;
 
 import com.codeup.diningreviewapi.Services.DiningReviewService;
 import com.codeup.diningreviewapi.models.DiningReview;
+import com.codeup.diningreviewapi.models.Restaurant;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/dining-reviews")
@@ -24,13 +24,41 @@ public class DiningReviewController {
     }
 
     @PostMapping
-    public DiningReview submitDiningReview(@RequestBody DiningReview diningReview){
+    public DiningReview submitDiningReview(@RequestBody DiningReview diningReview) {
 
         return diningReviewService.submitDiningReview(diningReview);
 
 
+    }
+
+    @GetMapping("/pending")
+    public List<DiningReview> getPendingReviews() {
+
+        return diningReviewService.getPendingDiningReviews();
+    }
+
+    @PutMapping("/{reviewId}")
+
+    public void approveOrRejectDiningReview(@PathVariable Long reviewId, @RequestParam boolean isApproved) {
+
+        diningReviewService.approveOrRejectDiningReview(reviewId, isApproved);
+
 
     }
+
+    // Unable to create this method at the moment since the restaurantService class does not exist.
+
+//    @GetMapping("/restaurant/{restaurantId}/approved")
+//    public List<DiningReview> getApprovedDiningReviewsForRestaurant(@PathVariable Long restaurantId){
+//
+//        // Fetch the Restaurant entity based on the restaurantId and pass it to the service method
+//
+//
+//
+//
+//
+//
+//    }
 
 
 }
---
+++ src/main/java/com/codeup/diningreviewapi/repositories/RestaurantRepository.java
@@ -0,0 +1,11 @@
+package com.codeup.diningreviewapi.repositories;
+
+public interface RestaurantRepository {
+
+
+
+
+
+
+
+}
