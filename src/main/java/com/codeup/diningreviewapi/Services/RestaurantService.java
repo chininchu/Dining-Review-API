bfc427a036b77f0f66285d1f6e4acc6c8cc3216f
chininchu <120990876+chininchu@users.noreply.github.com>
2023-08-08 21:14:50 +0500

    Created a method called getRestaurantById

--- src/main/java/com/codeup/diningreviewapi/Services/RestaurantService.java
+++ src/main/java/com/codeup/diningreviewapi/Services/RestaurantService.java
@@ -1,6 +1,9 @@
 package com.codeup.diningreviewapi.Services;
 
+import com.codeup.diningreviewapi.models.DiningUser;
 import com.codeup.diningreviewapi.models.Restaurant;
+import com.codeup.diningreviewapi.repositories.DiningReviewRepository;
+import com.codeup.diningreviewapi.repositories.DiningUserRepository;
 import com.codeup.diningreviewapi.repositories.RestaurantRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,16 +23,24 @@ public class RestaurantService {
 
     }
 
-    public Restaurant createRestaurant(Restaurant restaurant) {
+    public Restaurant createRestaurant(Restaurant restaurant, DiningUser diningUser) {
 
         // Check if a restaurant with the same name and zip code already exists
 
-        if (restaurantRepository.existsByNameAndDiningReviews_DiningUser_Zipcode(restaurant.getName(), restaurant.getZipCode())) {
+        if (restaurantRepository.existsByNameAndDiningReviews_DiningUser_Zipcode(restaurant.getName(), diningUser.getZipcode())) {
 
             throw new IllegalArgumentException("A restaurant with the same name and zip code already exists.");
         }
 
         return restaurantRepository.save(restaurant);
+
+
+    }
+
+    public Restaurant getRestaurantById(Long id){
+
+        return restaurantRepository.findById(id).orElse(null);
+
 
 
     }
