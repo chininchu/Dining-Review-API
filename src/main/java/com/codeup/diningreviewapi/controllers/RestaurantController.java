de5901cde1efbca9da8c0338f77e76eebfc773db
chininchu <120990876+chininchu@users.noreply.github.com>
2023-08-09 15:48:54 +0500

    Created a method called createRestaurant

--- src/main/java/com/codeup/diningreviewapi/controllers/RestaurantController.java
+++ src/main/java/com/codeup/diningreviewapi/controllers/RestaurantController.java
@@ -1,9 +1,12 @@
 package com.codeup.diningreviewapi.controllers;
 
 import com.codeup.diningreviewapi.Services.RestaurantService;
+import com.codeup.diningreviewapi.models.Restaurant;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,7 +26,24 @@ public class RestaurantController {
 
     }
 
+    @PostMapping
+    public ResponseEntity<?> createRestaurant(@RequestBody Restaurant restaurant) {
 
+        try {
+
+            Restaurant createdREstaurant = restaurantService.createRestaurant(restaurant);
+
+            return new ResponseEntity<>(createdREstaurant, HttpStatus.CREATED);
+
+        } catch (IllegalArgumentException e) {
+
+            return ResponseEntity.badRequest().body(e.getMessage());
+
+
+        }
+
+
+    }
 
 
 
